bae221095b3a48b5580c8e7432ec6dc2026e2958
Reyn Aria <reyn@buuuk.com>
2020-03-20 21:32:38 +0800

    first commit

---
+++ custom-hotspot.js
@@ -0,0 +1,21 @@
+// Hot spot creation function
+function hotspot(hotSpotDiv, args) {
+    hotSpotDiv.classList.add('custom-tooltip');
+    hotSpotDiv.classList.add('tooltip');
+    var span = document.createElement('span');
+    span.innerHTML = args;
+    hotSpotDiv.appendChild(span);
+    span.style.width = span.scrollWidth - 20 + 'px';
+    span.style.marginLeft = -(span.scrollWidth - hotSpotDiv.offsetWidth) / 2 + 'px';
+    span.style.marginTop = -span.scrollHeight - 12 + 'px';
+}
+function video_hotspot(hotSpotDiv, args) {
+    hotSpotDiv.classList.add('video-tooltip');
+    hotSpotDiv.classList.add('tooltip');
+    var span = document.createElement('span');
+    span.innerHTML = args;
+    hotSpotDiv.appendChild(span);
+    span.style.width = span.scrollWidth - 20 + 'px';
+    span.style.marginLeft = -(span.scrollWidth - hotSpotDiv.offsetWidth) / 2 + 'px';
+    span.style.marginTop = -span.scrollHeight - 12 + 'px';
+}
